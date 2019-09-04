151ad77a8df4d0da8229db9b3f6f7052bcbdd3cb
Vladimir Shubarin <vhundef@gmail.com>
2019-09-04 19:42:42 +0300

    Bot movement logic updated

--- Game/BotController.h
+++ Game/BotController.h
@@ -16,7 +16,128 @@ private:
         bool up{};
         bool down{};
         int chosenOption{0};
-    } canGo, canGoPrev;
+    } canGo;
+    int stopThreshold{0}, leftToGo{0};
+
+    void get_Direction() {
+        int tmp = GameObject::randIntInRange(1, 4);
+        canGo.left = controlledTank->checkIfCanGo(-20, -1);
+        canGo.right = controlledTank->checkIfCanGo(20, 1);
+        canGo.up = controlledTank->checkIfCanGo(-20, 2);
+        canGo.down = controlledTank->checkIfCanGo(20, -2);
+        if (!canGo.left && !canGo.right && !canGo.up && !canGo.down) canGo.chosenOption = 0;
+        if (!canGo.left && !canGo.right && !canGo.up && canGo.down) canGo.chosenOption = -2;
+        if (!canGo.left && !canGo.right && canGo.up && !canGo.down) canGo.chosenOption = 2;
+        if (!canGo.left && canGo.right && !canGo.up && !canGo.down) canGo.chosenOption = 1;
+        if (canGo.left && !canGo.right && !canGo.up && !canGo.down) canGo.chosenOption = -1;
+        if (!canGo.left && !canGo.right && canGo.up && canGo.down) {
+            if (tmp > 2) {
+                canGo.chosenOption = 2;
+            } else canGo.chosenOption = -2;
+        }
+        if (!canGo.left && canGo.right && !canGo.up && canGo.down) {
+            if (tmp > 2) {
+                canGo.chosenOption = 1;
+            } else canGo.chosenOption = -2;
+        }
+        if (!canGo.left && canGo.right && canGo.up && !canGo.down) {
+            if (tmp > 2) {
+                canGo.chosenOption = 1;
+            } else canGo.chosenOption = 2;
+        }
+        if (canGo.left && !canGo.right && !canGo.up && canGo.down) {
+            if (tmp > 2) {
+                canGo.chosenOption = -1;
+            } else canGo.chosenOption = -2;
+        }
+        if (canGo.left && !canGo.right && canGo.up && !canGo.down) {
+            if (tmp > 2) {
+                canGo.chosenOption = -1;
+            } else canGo.chosenOption = 2;
+        }
+        if (canGo.left && canGo.right && !canGo.up && !canGo.down) {
+            if (tmp > 2) {
+                canGo.chosenOption = -1;
+            } else canGo.chosenOption = 1;
+        }
+        if (!canGo.left && canGo.right && canGo.up && canGo.down) {
+            tmp = GameObject::randIntInRange(1, 3);
+            if (tmp == 1) {
+                canGo.chosenOption = 1;
+            } else if (tmp == 2)
+                canGo.chosenOption = 2;
+            else canGo.chosenOption = -2;
+        }
+        if (canGo.left && !canGo.right && canGo.up && canGo.down) {
+            tmp = GameObject::randIntInRange(1, 3);
+            if (tmp == 1) {
+                canGo.chosenOption = -1;
+            } else if (tmp == 2)
+                canGo.chosenOption = 2;
+            else canGo.chosenOption = -2;
+        }
+        if (canGo.left && canGo.right && !canGo.up && canGo.down) {
+            tmp = GameObject::randIntInRange(1, 3);
+            if (tmp == 1) {
+                canGo.chosenOption = 1;
+            } else if (tmp == 2)
+                canGo.chosenOption = -1;
+            else canGo.chosenOption = -2;
+        }
+        if (canGo.left && canGo.right && canGo.up && !canGo.down) {
+            tmp = GameObject::randIntInRange(1, 3);
+            if (tmp == 1) {
+                canGo.chosenOption = 1;
+            } else if (tmp == 2)
+                canGo.chosenOption = -1;
+            else canGo.chosenOption = 2;
+        }
+        if (canGo.left && canGo.right && canGo.up && canGo.down) {
+            tmp = GameObject::randIntInRange(1, 4);
+            if (tmp == 1) {
+                canGo.chosenOption = 1;
+            } else if (tmp == 2)
+                canGo.chosenOption = -1;
+            else if (tmp == 3) canGo.chosenOption = 2;
+            else canGo.chosenOption = -2;
+        }
+        cout<<"Will go"<<canGo.chosenOption<<endl;
+        get_purposedMovement();
+    }
+
+    void get_purposedMovement() {
+        stopThreshold = GameObject::randIntInRange(1, get_Distance());
+        cout<<"For "<<stopThreshold<<endl;
+    }
+
+    int get_Distance() {
+        int tmp{0};
+        switch (canGo.chosenOption) {
+            case -1:
+                while (controlledTank->checkIfCanGo(-tmp * 50, -1)) {
+                    tmp++;
+                }
+                break;
+            case 1:
+                while (controlledTank->checkIfCanGo(tmp * 50, 1)) {
+                    tmp++;
+                }
+                break;
+            case -2:
+                while (controlledTank->checkIfCanGo(-tmp * 50, 2)) {
+                    tmp++;
+                }
+                break;
+            case 2:
+                while (controlledTank->checkIfCanGo(tmp * 50, -2)) {
+                    tmp++;
+                }
+                break;
+            default:
+                cout << "WHAT THE HELL???" << endl;
+        }
+        return tmp;
+    }
 
 public:
     explicit BotController(Tank *ctrledTank) {
@@ -27,110 +148,22 @@ public:
     Tank *controlledTank;
 
     int makeMovementDecision() {
-        canGoPrev = canGo;
-        canGo.left = controlledTank->checkIfCanGo(-20, -1);
-        canGo.right = controlledTank->checkIfCanGo(20, 1);
-        canGo.up = controlledTank->checkIfCanGo(-20, 2);
-        canGo.down = controlledTank->checkIfCanGo(20, -2);
-        cout << "-------------------------------------" << endl;
-        cout << "Can Go Left:" << canGo.left << " Right:" << canGo.right << " Up:" << canGo.up << " Down:" << canGo.down
-             << endl;
-
-        int pathL{0}, pathR{0}, pathU{0}, pathD{0}, i = 1;
-
-        if (canGo.left)
-            while (controlledTank->checkIfCanGo(-pathL * 50, -1)) {
-                pathL++;
-            }
-        cout << "L " << pathL << endl;
-
-        if (canGo.right)
-            while (controlledTank->checkIfCanGo(pathR * 50, 1)) {
-                pathR++;
-            }
-        cout << "R " << pathR << endl;
-
-        if (canGo.up)
-            while (controlledTank->checkIfCanGo(-pathU * 50, 2)) {
-                pathU++;
-            }
-        cout << "U " << pathU << endl;
-
-        if (canGo.down)
-            while (controlledTank->checkIfCanGo(pathD * 50, -2)) {
-                pathD++;
-            }
-        cout << "D " << pathD << endl;
-
-
-        if (pathL > pathR && pathL > pathD && pathL > pathU) {
-            if (canGoPrev.chosenOption == 1 && canGo.right) {
-                canGo.chosenOption = 1;
-                return 1;
-            }
-            canGo.chosenOption = -1;
-            return -1;
+        leftToGo = abs(stopThreshold - get_Distance());
+        //cout << "Left to go: " << leftToGo << endl;
+        if (leftToGo == 0) {
+            get_Direction();
         }
-        if (pathR > pathL && pathR > pathD && pathR > pathU) {
-            if (canGoPrev.chosenOption == -1 && canGo.left) {
-                canGo.chosenOption = -1;
-                return -1;
-            }
-            canGo.chosenOption = 1;
-            return 1;
-        }
-        if (pathD > pathR && pathL > pathD && pathD > pathU) {
-            if (canGoPrev.chosenOption == 2 && canGo.up) {
-                canGo.chosenOption = 2;
-                return 2;
-            }
-            canGo.chosenOption = -2;
-            return -2;
+        return canGo.chosenOption;
 
-        }
-        if (pathU > pathR && pathU > pathD && pathL > pathU) {
-            if (canGoPrev.chosenOption == -2 && canGo.down) {
-                canGo.chosenOption = -2;
-                return -2;
-            }
-            canGo.chosenOption = 2;
-            return 2;
-
-        }
-        if (canGo.left && !canGo.right && !canGo.up && !canGo.down) {
-            canGo.chosenOption = -1;
-            return -1;
-        }
-        if (canGo.right && !canGo.left && !canGo.up && !canGo.down) {
-            canGo.chosenOption = 1;
-            return 1;
-        }
-        if (canGo.up && !canGo.left && !canGo.right && !canGo.down) {
-            canGo.chosenOption = 2;
-            return 2;
-        }
-        if (canGo.down && !canGo.up && !canGo.left && !canGo.right) {
-            canGo.chosenOption = -2;
-            return -2;
-        }
-        if (canGo.down && canGo.right) {
-            if (GameObject::randIntInRange(1, 100) == 10) {
-                canGo.chosenOption = -2;
-                return -2;
-            } else {
-                canGo.chosenOption = 1;
-                return 1;
-            }
-        }
     }
 
     void moveTank() {
         controlledTank->setMovementDirection(makeMovementDecision());
     }
 
-    /*  GameObject::coords getBotCoords(){
-          return controlledTank.location;
-      }*/
+/*  GameObject::coords getBotCoords(){
+      return controlledTank.location;
+  }*/
 };
 
 
